a48c0842512a0a24c2b28c87e1753f1f4b0f4e39
Zedmous <zedmous@gmail.com>
2024-07-09 22:58:46 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'iujo-fe-general-2024-1';
+}

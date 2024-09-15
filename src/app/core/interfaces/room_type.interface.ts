9879735433448ba809253e533ceb464bca84aa44
Ricardo Palacios <154641725+Ricardopalacios32@users.noreply.github.com>
2024-09-15 18:38:26 -0400

    Revert "priemros componentes"

--- src/app/core/interfaces/room_type.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface RoomTypeInterface{
-    id?:number;
-    name:string;
-    status?:boolean;
-    createdAt:Date;
-    updatedAt:Date;
-    deletedAt:Date;
-    action?:string;
-}
--- src/app/pages/dashboard/hotels/hotels.module.ts
+++ src/app/pages/dashboard/hotels/hotels.module.ts
@@ -4,16 +4,12 @@ import { CommonModule } from '@angular/common';
 import { HotelsRoutingModule } from './hotels-routing.module';
 import { HotelsHomeComponent } from './hotels-home/hotels-home.component';
 import { HotelsReportsComponent } from './hotels-reports/hotels-reports.component';
-import { ListRoomTypesComponent } from './room_types/list-room-types/list-room-types.component';
-import { FormRoomTypesComponent } from './room_types/form-room-types/form-room-types.component';
 
 
 @NgModule({
   declarations: [
     HotelsHomeComponent,
-    HotelsReportsComponent,
-    ListRoomTypesComponent,
-    FormRoomTypesComponent
+    HotelsReportsComponent
   ],
   imports: [
     CommonModule,

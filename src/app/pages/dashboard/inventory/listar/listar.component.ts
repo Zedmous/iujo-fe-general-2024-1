e61f35df341022f15f5ac57b783e01da576ac0b4
Yeison Perez <yeisonp323@gmail.com>
2024-09-16 03:41:09 -0400

    ajuste

--- src/app/pages/dashboard/inventory/listar/listar.component.ts
+++ src/app/pages/dashboard/inventory/listar/listar.component.ts
@@ -52,7 +52,7 @@ export class ListarComponent {
     // Create 100 users
     const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
 
-    // Assign the data to the data source for the table to render
+    // Assign the data to the data source for the table to rende
     this.dataSource = new MatTableDataSource(ELEMENT_DATA);
   }
 

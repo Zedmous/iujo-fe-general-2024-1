be1207ae60c257f8a6e7be1b53ccf3b7310f3614
ernalyt <ernalytvasquez25@gmail.com>
2024-09-20 14:06:55 -0400

    casi listo

--- src/app/pages/dashboard/rrhh/rrhh.module.ts
+++ src/app/pages/dashboard/rrhh/rrhh.module.ts
@@ -11,8 +11,12 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -31,7 +35,12 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+    MatIconModule,
+    MatToolbarModule,
+    ReactiveFormsModule
   ]
 })
 export class RrhhModule {}
